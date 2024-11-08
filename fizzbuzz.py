9f973565aee52860b6e73ef980dc2debb16d2960
khan <hasnatgamer93@gmail.com>
2024-11-08 17:30:54 +0100

    fichier fizzbuzz.py modifié

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,12 @@
 def affiche(n1, n2):
     result = ""
     for i in range(n1, n2 + 1):
+        if i % 15 == 0:
             result += "FrisBee "
-return
+        elif i % 3 == 0:
+            result += "Fizz "
+        elif i % 5 == 0:
+            result += "Buzz "
+        else:
+            result += str(i) + " "
+    return result.strip()
