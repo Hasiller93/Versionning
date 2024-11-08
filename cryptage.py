5f458ab10fd99d22c7787cdfb63ec76612fb6e8f
khan <hasnatgamer93@gmail.com>
2024-11-08 17:40:21 +0100

    fonction crypt qui fonctionne avec fichiers modifiés

--- cryptage.py
+++ cryptage.py
@@ -1,5 +1,10 @@
 import string
 
 
-def crypt(message):
-	return ""
+def crypt(message, pas=1):
+    caracteres = string.ascii_letters + string.punctuation + string.digits + " "
+    return ''.join([caracteres[(caracteres.index(c) + pas) % len(caracteres)] for c in message])
+
+def decrypt(message, pas=1):
+    caracteres = string.ascii_letters + string.punctuation + string.digits + " "
+    return ''.join([caracteres[(caracteres.index(c) - pas) % len(caracteres)] for c in message])
--- test_cryptage.py
+++ test_cryptage.py
@@ -3,3 +3,11 @@ from cryptage import crypt, decrypt
 
 class TestCrypt(unittest.TestCase):
     def test_crypt(self):
+        self.assertEqual(crypt("abc"), "bcd")
+    def test_crypt_avec_pas(self):
+        self.assertEqual(crypt("abc", 1), "bcd")
+    def test_decrypt(self):
+        self.assertEqual(decrypt("bcd", 1), "abc")
+
+if __name__ == "__main__":
+    unittest.main()
